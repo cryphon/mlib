5e594e777ef612d75bbb0d799a283e954631af0b
cryphon <yvanroes@gmail.com>
2024-07-31 22:14:37 +0000

    remove testing functions

--- main.cpp
+++ main.cpp
@@ -3,39 +3,6 @@
 #include <mlib/arithmetic.hpp>
 #include <testing/test_util.hpp>
 
-int test_add() {
-    int a = 10, b = 20;
-    int res = add(a, b), exp = 30;
-    char* description = "Add numbers one number to another";
-    equals(description, res, exp);
-    return 0;
-}
-
-int test_sub() {
-    int a = 10, b = 20;
-    int res = sub(b, a), exp = 10;
-    char* description = "Substract a number from another";
-    equals(description, res, exp);   
-    return 0;
-}
-
-int test_mul() {
-    int a = 10, b = 20;
-    int res = multiply(a, b), exp = 200;
-    char* description = "Multiply a number with another";
-    equals(description, res, exp);
-    return 0;
-}
-
-int test_div() {
-   int a = 10, b = 20;
-   double res = divide(a, b), exp = 0.5;
-   char* description = "Divide one number by another";
-   equals(description, res, exp);
-   return 0;
-}
-
-
 int main(void) {
     int a = 10, b = 20;
 
@@ -49,11 +16,7 @@ int main(void) {
     std::cout << "[sqrt]: " << m_sqrt(b) << "\n";
     std::cout << "[abs]: " << m_abs(-b) << "\n"; 
     std::cout << "[log]: " << m_log(b) << "\n";
-
-    test_add();
-    test_sub();
-    test_mul();
-    test_div();
+   
     return 0;
 }
 
